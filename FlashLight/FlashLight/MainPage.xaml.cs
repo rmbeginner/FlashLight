f19614e3a9b6197f33fffac22e87a67e10399509
UserIT <UserIT@ITADMIN>
2022-12-30 17:30:33 +0800

    Add project files.

---
+++ FlashLight/FlashLight/MainPage.xaml.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Essentials;
+using Xamarin.Forms.Xaml;
+
+namespace FlashLight
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ButtonOn_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOnAsync();
+            }
+            catch (Exception)
+            {
+            }
+
+
+        }
+
+        private async void ButtonOff_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                await Flashlight.TurnOffAsync();
+            }
+            catch (Exception)
+            {
+            }
+        }
+    }    
+}
